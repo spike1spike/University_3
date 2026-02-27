c06b228c753af6ca63dcc7a8099fca104ec16279
spike1spike <43131663+spike1spike@users.noreply.github.com>
2026-02-27 12:31:46 +0300

    Add files via upload

---
+++ Practice/Practice_2/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <clocale>﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+	if (n % 2 == 1) {
+		n++;
+	}
+	double h = (b - a) / n;
+	double sum = f(a) + f(b);
+
+	for (int i = 1; i < n; i += 2) {
+		sum += 4 * f(a + i * h);
+	}
+	for (int i = 2; i < n - 1; i += 2) {
+		sum += 2 * f(a + i * h);
+	}
+
+	return (h / 3) * sum;
+}
+
+double function_1(double x) {
+	return 4 + x;
+}
+
+double function_2(double x) {
+	return 4 - x;
+}
+
+int main() {
+	setlocale(LC_CTYPE, "rus");
+
+	for (int i = 2; i < 11; i += 2) {
+		double result_1 = simpson_rule(function_1, -4, 0, i);
+		double result_2 = simpson_rule(function_2, 0, 4, i);
+		double result = result_1 + result_2;
+		cout << "Численное значение интеграла при n = " << i << ": " << result << endl;
+	}
+
+	return 0;
+}
---
+++ Practice/Practice_2/UnitTest1.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double result = simpson_rule(function, -4, 4, 2);
+			Assert::AreEqual(result, 16, 6, L"Значения совпали с точностью 6");
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			double result = simpson_rule(function, -4, 4, 6);
+			Assert::AreEqual(result, 16, 1, L"Значения совпали с точностью 1");
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			double result = simpson_rule(function, -4, 4, 10);
+			Assert::AreEqual(result, 16, 0.5, L"Значения совпали с точностью 0.5");
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			double result = simpson_rule(function, -4, 4, 102);
+			Assert::AreEqual(result, 16, 0.05, L"Значения совпали с точностью 0.05");
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			double result = simpson_rule(function, -4, 4, 502);
+			Assert::AreEqual(result, 16, 0.0002, L"Значения совпали с точностью 0.0002");
+		}
+	};
+}
