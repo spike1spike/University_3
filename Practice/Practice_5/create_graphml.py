3ddcb3353089410f568be25f54fbfb44aa49056d
spike1spike <43131663+spike1spike@users.noreply.github.com>
2026-06-05 01:03:14 +0300

    Delete Practice/Practice_5/create_graphml.py

--- Practice/Practice_5/create_graphml.py
+++
@@ -1,6 +0,0 @@
-import osmnx as ox
-
-# Скачиваем граф по названию города
-G = ox.graph_from_place('Мадрид, Испания', network_type='drive')
-# Сохраняем в файл для последующего использования
-ox.save_graphml(G, 'moscow_road_network.graphml')
